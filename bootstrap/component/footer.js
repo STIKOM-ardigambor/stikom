267175a79568a33e1eca932389e32a350e6c10bf
STIKOM-ardigambor <radenarifhadisumitro@gmail.com>
2020-07-10 23:01:45 +0700

    publish folder

---
+++ bootstrap/component/footer.js
@@ -0,0 +1,12 @@
+export default ()=>{
+    return(
+        <div>
+            <div className="card text-center">
+                <div class="card-header">
+                <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
