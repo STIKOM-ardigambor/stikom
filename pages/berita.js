4e62d05b309c8123630cd87c5f2ca515fd77d7d8
STIKOM-ardigambor <radenarifhadisumitro@gmail.com>
2020-07-12 08:04:41 +0700

    publish folder

---
+++ pages/berita.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import Layout from '../component/template'
+export default class extends Component{
+    render(){
+        return(
+            <div>
+                <Layout>
+                    
+                </Layout>
+            </div>
+        )
+    }
+}
